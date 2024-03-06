6176dfbc04d4cd18f6fa59bea380eedcd7b13c57
Froches <michaelfavour418@gmail.com>
2024-03-06 15:32:07 +0000

    memethof properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+// Code to get full budget
+//
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars() {
+      return `$${income}`;
+    },
+    getIncomeInEuros() {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
