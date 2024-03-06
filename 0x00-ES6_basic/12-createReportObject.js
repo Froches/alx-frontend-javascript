79eb14fca0e1b244e7ff5af018a66a5458c8714c
Froches <michaelfavour418@gmail.com>
2024-03-06 17:22:55 +0000

    crrcreate report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+// Create a Report Object
+//
+export default function createReportObject(employeesList) {
+  const reportObj = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return reportObj;
+}
