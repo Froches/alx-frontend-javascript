b268c01ab4256e52c41bbad3cb45d7e2e49c8848
Froches <michaelfavour418@gmail.com>
2024-03-08 22:25:34 +0000

    class, getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+// New class
+//
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = this.newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = this.newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('students must be an array');
+    }
+    this._students = this.newStudents;
+  }
+}
