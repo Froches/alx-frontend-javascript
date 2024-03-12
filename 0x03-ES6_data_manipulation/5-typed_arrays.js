64b946e7fd068413053d25a84d117ff80c0f3687
Froches <michaelfavour418@gmail.com>
2024-03-12 15:52:39 +0000

    typed array fixed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,8 +4,11 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
+
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  int8Array[position] = value;
-  return buffer;
+  const dadaView = new DataView(buffer);
+
+  dadaView.setInt8(position, value);
+
+  return dadaView;
 }
