e73a233fdb9f674287aa4c0a4ef008633458877f
Froches <michaelfavour418@gmail.com>
2024-03-12 15:40:52 +0000

    typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+// Returns a new ArrayBuffer with an Int8 value at a specific position
+//
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+  return buffer;
+}
