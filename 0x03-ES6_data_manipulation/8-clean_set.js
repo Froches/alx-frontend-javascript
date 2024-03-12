c1bdb86f7b69651d2c5b70269a971417e6b5fc8c
Froches <michaelfavour418@gmail.com>
2024-03-12 21:25:57 +0000

    startstring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 // Returns a string of all the set values that start with a specific string
 //
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
