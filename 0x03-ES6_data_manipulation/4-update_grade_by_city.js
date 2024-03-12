f969f77814cf46c43bc0458f26edc00660418d59
Froches <michaelfavour418@gmail.com>
2024-03-12 15:12:56 +0000

    update grade by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+// Returns an array of students for a specific city with their new grade
+//
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const studentsCity = studentList.filter((student) => student.location === city);
+
+  const updatedStudents = studentsCity.map((student) => {
+    const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+    const updatedGrade = studentGrade ? studentGrade.grade : 'N/A';
+
+    return { ...student, grade: updatedGrade };
+  });
+  return updatedStudents;
+}
